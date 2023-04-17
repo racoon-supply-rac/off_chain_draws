aa0a5088684a3e2aeb93d63d3d9d9a8ae9ae7b04
RACSUP <racoon,.supply.nft@gmail.com>
2023-04-17 09:06:00 -0400

    new giveaway draw

---
+++ empawrium_draws/april142023_betaannouncement_nft_500k.py
@@ -0,0 +1,24 @@
+# https://twitter.com/RacoonSupply/status/1646877601250566145
+
+# Prizes: 1 Cyber Rac & 500k $HUAHUA
+
+import numpy as np
+
+list_of_participants = ['@chrissss0007', '@Dennistough', '@KHUNFI', '@0xponci', '@RoboVerseWeb3', '@SalihKasm4', '@botsandaidev', '@MaxPain47209603', '@Keios23', '@AAAMetaHuahua', '@Evansmighty1', '@CRYPTOFUTURE019', '@NicchiloSpodo', '@jbertter', '@DB68186118', '@dew_on_roses', '@Pegasus', '@cryptow00t', '@cryptodeeek', '@blocktech_dev', '@OyinsSimply', '@TimiNewton', '@SiriusSquatch', '@FYCUSVLADIMIR', '@nanks_d', '@Ghostboyxy', '@denfatuf', '@shrute_buck', '@ChihuahuaChain', '@MarianOko74', '@masculus_', '@junioroyindeny1', '@pianonice1', '@Firefly_808', '@Oscarp440trader', '@Bl_ckkky', '@cxcxdz', '@Ownyourplace', '@CyptoShaman', '@NftChihuahua', '@Sokin95050947', '@MoonixLoL', '@Giorgos_zz', '@RAC_FM_Spaces', '@Rarma_', '@DrBittcoin', '@Tedstweets4', '@Bitjoint2', '@mathgril', '@stahly85', '@emileadipura', '@Deenanks2', '@Thanasi69473216', '@superman__171', '@Snowflake0156', '@Madman_Crypto_', '@hecktada', '@spicygurl444', '@HitWithRock1', '@Ignat233', '@HUAHUA_S', '@EyeofProvidenc8', '@DonDon14321', '@Welding_Cosmos', '@clayton6282', '@The4thIR', '@KleomedesDAO', '@us3l3ssch1', '@WHiTeRHiNo_420', '@Raswid', '@alsahhim31', '@GregWeld', '@NonWaruth', '@mejiamarilyn33', '@brucemanRAC', '@LuisDake', '@0xSenCom', '@surestboy10', '@SHIVyaA2Z', '@soswe9', '@cryptotosis', '@cryptozambiano', '@Yiouhou2', '@aldibradiiiiiii', '@denthinker', '@66Mhz', '@agentmcgarrity', '@Cessyderama', '@HUAHUA_S6', '@lucasdosrl', '@Mickeypirson', '@BayoSister', '@gigibitcoin', '@X269IndigoMAFIA', '@lilgainzz', '@akunokikori1', '@Patvidgi', '@skrillaaaaaaaaa', '@aji3003', '@_Manimal', '@smartbadboi', '@The5thForce0_0', '@Branzahm', '@CryptoNewbieArg', '@Mich_crypto1989', '@Esteerluv', '@nub310072', '@1989MUERTE1989', '@peaceau', '@nimbus9145', '@KemsiAwiz', '@poubenijosephi1', '@homestead_cosmo', '@TheSherriff24', '@DizzleSocial', '@doglife420', '@Scott59021942', '@donlu55']
+
+# Seed has been taken from random.org: 355032148 ->
+# 355032148
+# Min: 1, Max: 999999999
+# 2023-04-17 13:01:55 UTC
+
+seed = 355032148
+np.random.seed(seed)
+
+winners = np.random.choice(list_of_participants, 2, False)
+print(f"Cyber Rac NFT winner is: {winners[0]}")
+print(f"500k $HUAHUA winner is: {winners[1]}")
+print(f"Congrats :D")
+
+# Cyber Rac NFT winner is: @agentmcgarrity
+# 500k $HUAHUA winner is: @Firefly_808
+# Congrats :D
